30ee1d6614c784440ae12dd717ab2e6fa77071ec
DRAGONDAR222 <darioluce.dx@gmail.com>
2025-03-04 19:21:09 +0100

    .

--- funzioni secondo es.py
+++ funzioni secondo es.py
@@ -10,3 +10,4 @@ def substract(a:int,b:int):
 
 # "return" è il valore FINALE che viene attribuito come risultato della "funzione" (indipendentemente dai processi avvenuti nella funzione stessa)
 # senza il "return" avremmo soltato il PROCESSO senza alcun esito
+# utilizzando la "funzione" in qualsiasi "processo" questa corrisponderà direttamente al suo corrispettivo "return" (i processi intermedi è come se avenissero dietro le quinte)
