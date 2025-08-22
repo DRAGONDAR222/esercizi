b98f2f5c2bd22dafe57807c024c2608529200093
DRAGONDAR222 <darioluce.dx@gmail.com>
2025-08-22 20:13:00 +0200

    .

--- sql/officine.sql
+++ sql/officine.sql
@@ -2,11 +2,8 @@ CREATE DOMAIN Stringa AS varchar(255);
 
 CREATE DOMAIN CodiceFiscale AS char(16);
 
-CREATE DOMAIN posinteger AS integer CHECK (VALUE >= 0);
-
-CREATE DOMAIN Targa AS varchar(10) CHECK (VALUE ~ '^[A-Z]{2}[0-9]{3}[A-Z]{2}$');
-
-CREATE DOMAIN Telefono AS varchar(20) CHECK (VALUE ~ '^\+?[0-9\s\-\(\)]{6,20}$');
+CREATE DOMAIN posinteger AS integer
+CHECK (value >= 0);
 
 CREATE TYPE Indirizzo AS (
     via Stringa,
@@ -14,34 +11,40 @@ CREATE TYPE Indirizzo AS (
     CAP integer
 );
 
+CREATE DOMAIN Targa AS varchar(10)
+CHECK (VALUE ~ '^[A-Z]{2}[0-9]{3}[A-Z]{2}$');
+
+CREATE DOMAIN Telefono AS varchar(20)
+CHECK (value ~ '^\+?[0-9\s\-\(\)]{6,20}$');
 
 CREATE TABLE Proprietario(
-    id SERIAL PRIMARY KEY
+    id integer PRIMARY KEY
 );
 
 CREATE TABLE Direttore(
-    id SERIAL PRIMARY KEY,
+    id integer PRIMARY KEY,
     data_nascita date NOT NULL
 );
 
 CREATE TABLE Officina(
-    id SERIAL PRIMARY KEY,
+    id integer NOT NULL,
     nome Stringa NOT NULL,
     indirizzo Indirizzo NOT NULL,
     n_dipendenti posinteger NOT NULL,
     direttore integer NOT NULL,
+    PRIMARY KEY(id, direttore),
     FOREIGN KEY(direttore) REFERENCES Direttore(id)
 );
 
 CREATE TABLE Dipendente(
-    id SERIAL PRIMARY KEY,
+    id integer PRIMARY KEY,
     anni_servizio integer NOT NULL,
     officina integer NOT NULL,
     FOREIGN KEY(officina) REFERENCES Officina(id)
 );
 
 CREATE TABLE Persona(
-    id SERIAL PRIMARY KEY,
+    id integer PRIMARY KEY,
     nome Stringa NOT NULL,
     cf CodiceFiscale NOT NULL,
     indirizzo Indirizzo NOT NULL,
@@ -55,34 +58,34 @@ CREATE TABLE Persona(
 );
 
 CREATE TABLE Veicolo(
-    targa Targa PRIMARY KEY,
     modello Stringa NOT NULL,
     tipo Stringa NOT NULL,
+    targa Targa PRIMARY KEY,
     anno_I integer NOT NULL,
     proprietario integer NOT NULL,
     FOREIGN KEY(proprietario) REFERENCES Proprietario(id)
 );
 
 CREATE TABLE In_corso(
-    id SERIAL PRIMARY KEY
+    id integer PRIMARY KEY
 );
 
 CREATE TABLE Terminata(
-    id SERIAL PRIMARY KEY,
+    id integer PRIMARY KEY,
     data_c date NOT NULL,
     ora_c integer NOT NULL
 );
 
 CREATE TABLE Riparazione(
-    id SERIAL NOT NULL,
-    veicolo Targa NOT NULL,
     codice Stringa NOT NULL,
     data_A date NOT NULL,
     ora_A integer NOT NULL,
+    id integer NOT NULL,
     in_corso integer,
     terminata integer,
+    veicolo Targa NOT NULL,
     PRIMARY KEY(id, veicolo),
-    FOREIGN KEY(in_corso) REFERENCES In_corso(id),
     FOREIGN KEY(terminata) REFERENCES Terminata(id),
+    FOREIGN KEY(in_corso) REFERENCES In_corso(id),
     FOREIGN KEY(veicolo) REFERENCES Veicolo(targa)
 );
