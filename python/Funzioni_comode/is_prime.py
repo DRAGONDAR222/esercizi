3da98d2304f3c8ff53cdb074b1e6afe044acde9c
DRAGONDAR222 <darioluce.dx@gmail.com>
2025-09-27 12:52:17 +0200

    .

---
+++ python/Funzioni_comode/is_prime.py
@@ -0,0 +1,11 @@
+def is_prime(x):
+    if x <= 1:
+        return False
+    
+    is_prime = True
+    for j in range(2, int(x ** 0.5) + 1):
+        if x % j == 0:
+            is_prime = False
+            break
+    
+    return is_prime
--- python/code-dragons/Capitolo_6/30_tesoriera_ottimale.py
+++ python/code-dragons/Capitolo_6/30_tesoriera_ottimale.py
@@ -1,2 +1,21 @@
 '''Alla Tesoreria Ottimale, ogni tributo va minimizzato. Calcola con `min_coins(amount, coins)`: restituisci il **numero minimo** di monete per ottenere `amount`; se impossibile, torna `1_000_000_000`. Mantieni la firma e lascia che i test si aprano.'''
 
+def min_coins(amount: int, coins: list[int]) -> int:
+
+    if amount < 0:
+        return'1_000_000_000'
+    
+    elif amount == 0:
+        return 0
+
+    coins_dict:dict = {}
+    count_coins:int = 0
+
+    for coin in sorted(coins,reverse = True):
+        if coin < amount:
+            coins_dict[coin] = amount // coin          
+
+    while amount > 0:
+        max_coin: int = min(coins_dict, key = lambda x:coins_dict[x])
+        
+ # tentativo non funzionante
