dff3b9ac3db68461177f169b18a14b15c65840c3
DRAGONDAR222 <darioluce.dx@gmail.com>
2025-11-25 19:10:47 +0100

    .

---
+++ React/seconda-app/src/Parcheggio/Gestore_parcheggio.jsx
@@ -0,0 +1,5 @@
+// Dato un un forma di inserimento di utente targa. Implementar eun sistema che inserisca gli utenti e li 
+// visualizzi in una tabella. se la targa è già stata inserita non è possibile inserirla nuovamente
+
+// Farlo funzionar ein un componente unico come primo step. Come secondo step dividerlo nei seguenti 
+// compnenti: TargaForm, ListaTarghe, ItemTarga, TargaAuto che è il componente principale
